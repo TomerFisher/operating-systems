d40167faeaf853be627378fca9ef66e9879cabe8
Tomer Fisher <fisher.tomer@gmail.com>
2022-03-30 03:29:37 +0300

    add functions to check the command type

--- Ex2/myshell.c
+++ Ex2/myshell.c
@@ -6,21 +6,62 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-// arglist - a list of char* arguments (words) provided by the user
-// it contains count+1 items, where the last item (arglist[count]) and *only* the last is NULL
-// RETURNS - 1 if should continue, 0 otherwise
+int isBackground(int count, char** arglist)
+{
+	if (count > 1 && strcmp(arglist[count-1], "&") == 0) {
+		arglist[count-1] = NULL;
+		return 1;
+	}
+	return 0;
+}
+
+int isOutputRedirection(int count, char** arglist)
+{
+	if (count > 2 && strcmp(arglist[count-2], ">>") == 0) {
+		arglist[count-2] = NULL;
+		return 1;
+	}
+	return 0;
+}
+
+int isPiping(int count, char** arglist, int* pipe_index)
+{
+	for (int index = 1; index < count-1; index++){
+		if (strcmp(arglist[index], "|") == 0) {
+			arglist[index] = NULL;
+			pipe_index = index;
+			return 1;
+		}
+	}
+	return 0;
+}
+
 int process_arglist(int count, char** arglist)
 {
-	int pid, status_code, pipe_index;
+	int backgroundFlag, outputRedirectionFlag, pipingFlag, pipe_index;
+	
+	// check is the command is regular, background, output redirection or piping
+	backgroundFlag = isBackground(count, arglist);
+	outputRedirectionFlag = isOutputRedirection(count, arglist);
+	pipingFlag = isPiping(count, arglist, &pipe_index);
+	
+	
+	int pid, status_code, pipe_index, new_pid;
 	int background = 0, output_redirection = 0, piping = 0;
+	
+	// check if there is '&' in arglist
 	if (count > 1 && strcmp(arglist[count-1], "&") == 0) {
 		background = 1;
 		arglist[count-1] = NULL;
 	}
+
+	// check if there is '>>' in arglist
 	else if (count > 2 && strcmp(arglist[count-2], ">>") == 0) {
 		output_redirection = 1;
 		arglist[count-2] = NULL;
 	}
+
+	// check is there is '|' in arglist
 	else if (count > 2) {
 		for (int index = 1; index < count-1; index++){
 			if (strcmp(arglist[index], "|") == 0) {
@@ -41,7 +82,7 @@ int process_arglist(int count, char** arglist)
 		}
 		else if (piping == 1){
 			printf("#this command have pipe\n");
-			int p[2], new_pid;
+			int p[2];
 			pipe(p);
 			new_pid = fork();
 			if (new_pid > 0) {
@@ -49,7 +90,7 @@ int process_arglist(int count, char** arglist)
 				status_code = execvp(arglist[0], arglist);
 			}
 			else {
-				waitpid(new_pid);
+				//waitpid(new_pid);
 				dup2(p[0], 0);
 				status_code = execvp(arglist[pipe_index+1], &arglist[pipe_index+1]);
 			}
@@ -61,8 +102,10 @@ int process_arglist(int count, char** arglist)
 	else {
 		if (background == 0) {
 			printf("#this regular command\n");
+			if (piping == 1){
+				waitpid(new_pid);
+			}
 			waitpid(pid);
-
 		}
 		else
 			printf("#this backgroup command\n");
@@ -70,7 +113,6 @@ int process_arglist(int count, char** arglist)
 	return 1;
 }
 
-// prepare and finalize calls for initialization and destruction of anything required
 int prepare(void)
 {
 	return 0;
