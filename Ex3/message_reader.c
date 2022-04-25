93636332fa78284b2b4715f9da28e6e0e278515f
Tomer Fisher <fisher.tomer@gmail.com>
2022-04-26 01:47:53 +0300

    add message sender & reader

--- Ex3/message_reader.c
+++ Ex3/message_reader.c
@@ -5,8 +5,35 @@
 #include <sys/ioctl.h>  /* ioctl */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(int argc, char** argv)
 {
-  
+  int file_descriptor, message_length;
+  unsigned long channel_id;
+  char buffer[BUF_LEN];
+  if (argc != 3) {
+	  perror("Error: invalid number of arguments");
+	  exit(1);
+  }
+  file_descriptor = open(argv[1], O_RDONLY);
+  if (file_descriptor < 0) {
+	  perror("Error: could not open file descriptor");
+	  exit(1);
+  }
+  channel_id = atoi(argv[2]);
+  if (ioctl(file_desc, MSG_SLOT_CHANNEL, channel_id) != SUCCESS) {
+	  perror("Error: ioctl failed");
+	  exit(1);
+  }
+  message_length = read(file_desc, buffer, BUF_LEN);
+  if (message_length < 0) {
+	  perror("Error: read failed");
+	  exit(1);
+  }
+  close(file_descriptor);
+  if (write(1, buffer, message_length) < 0) {
+	  perror("Error: write message to stdout failed");
+  }
+  return 0;
 }
--- Ex3/message_sender.c
+++ Ex3/message_sender.c
@@ -5,8 +5,31 @@
 #include <sys/ioctl.h>  /* ioctl */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(int argc, char** argv)
 {
-  
+  int file_descriptor, message_length;
+  unsigned long channel_id;
+  if (argc != 4) {
+	  perror("Error: invalid number of arguments");
+	  exit(1);
+  }
+  file_descriptor = open(argv[1], O_WRONLY);
+  if (file_descriptor < 0) {
+	  perror("Error: could not open file descriptor");
+	  exit(1);
+  }
+  channel_id = atoi(argv[2]);
+  if (ioctl(file_desc, MSG_SLOT_CHANNEL, channel_id) != SUCCESS) {
+	  perror("Error: ioctl failed");
+	  exit(1);
+  }
+  message_length = strlen(argv[3]);
+  if (write(file_desc, argv[3], message_length) != message_length) {
+	  perror("Error: write failed");
+	  exit(1);
+  }
+  close(file_descriptor);
+  return 0;
 }
