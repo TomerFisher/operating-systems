6c31e857812d81448ad9fcb7cd611f531ef9bf72
Tomer Fisher <fisher.tomer@gmail.com>
2022-04-29 12:32:59 +0300

    add kernel module implementation

--- Ex3/message_slot.c
+++ Ex3/message_slot.c
@@ -19,115 +19,125 @@ MODULE_LICENSE("GPL");
 //Our custom definitions of IOCTL operations
 #include "message_slot.h"
 
-struct chardev_info
-{
-  spinlock_t lock;
-};
+struct channel {
+	long id;
+	char message[BUF_LEN];
+	int message_length;
+}
 
-// used to prevent concurent access into the same device
-static int dev_open_flag = 0;
+struct channel_node {
+	struct channel data;
+	struct channel_node *next;
+}
 
-static struct chardev_info device_info;
+struct file_info {
+	struct channel current_channel;
+	int minor;
+}
+
+struct channel_node message_slot_channels[256];
 
 // The message the device will give when asked
 static char the_message[BUF_LEN];
-
-//Do we need to encrypt?
-static int encryption_flag = 0;
 
 //================== DEVICE FUNCTIONS ===========================
 static int device_open( struct inode* inode,
                         struct file*  file )
 {
-  unsigned long flags; // for spinlock
-  printk("Invoking device_open(%p)\n", file);
-
-  // We don't want to talk to two processes at the same time
-  spin_lock_irqsave(&device_info.lock, flags);
-  if( 1 == dev_open_flag )
-  {
-    spin_unlock_irqrestore(&device_info.lock, flags);
-    return -EBUSY;
+  int minor = iminor(inode);
+  file_info* data = kmalloc(sizeof(file_info), GFP_KERNEL);
+  if (data == NULL) {
+	  return -1;
   }
-
-  ++dev_open_flag;
-  spin_unlock_irqrestore(&device_info.lock, flags);
+  data->minor = minor;
+  data->current_channel = message_slot_channels[minor-1]->channel;
+  file->private_data = (void*) data;
   return SUCCESS;
 }
 
-//---------------------------------------------------------------
-static int device_release( struct inode* inode,
-                           struct file*  file)
-{
-  unsigned long flags; // for spinlock
-  printk("Invoking device_release(%p,%p)\n", inode, file);
-
-  // ready for our next caller
-  spin_lock_irqsave(&device_info.lock, flags);
-  --dev_open_flag;
-  spin_unlock_irqrestore(&device_info.lock, flags);
-  return SUCCESS;
+static int device_release(struct inode* inode, struct file*  file) {
+	kfree(file->private_data);
+	return SUCCESS;
 }
 
-//---------------------------------------------------------------
-// a process which has already opened
-// the device file attempts to read from it
 static ssize_t device_read( struct file* file,
                             char __user* buffer,
                             size_t       length,
                             loff_t*      offset )
 {
-  // read doesnt really do anything (for now)
-  printk( "Invocing device_read(%p,%ld) - "
-          "operation not supported yet\n"
-          "(last written - %s)\n",
-          file, length, the_message );
-  //invalid argument error
-  return -EINVAL;
+  if (file->private_data->current_channel == NULL) {
+	  errno = EINVAL;
+	  return -1;
+  }
+  if (file->private_data->current_channel->message == NULL) {
+	  errno = EWOULDBLOCK;
+	  return -1;
+  }
+  if (length < file->private_data->current_channel->message_length) {
+	  errno = ENOSPC;
+	  return -1;
+  }
+  for (int i = 0; i < file->private_data->current_channel->message_length; i++) {
+	  if (put_user(file->private_data->current_channel->message[i], buffer + i) != 0)
+		  return -1;
+  }
+  return file->private_data->current_channel->message_length;
 }
 
-//---------------------------------------------------------------
-// a processs which has already opened
-// the device file attempts to write to it
 static ssize_t device_write( struct file*       file,
                              const char __user* buffer,
                              size_t             length,
                              loff_t*            offset)
 {
-  int i;
-  printk("Invoking device_write(%p,%ld)\n", file, length);
-  for( i = 0; i < length && i < BUF_LEN; ++i )
-  {
-    get_user(the_message[i], &buffer[i]);
-    if( 1 == encryption_flag )
-      the_message[i] += 1;
+  if (file->private_data->current_channel == NULL) {
+	  errno = EINVAL;
+	  return -1;
   }
- 
-  // return the number of input characters used
-  return i;
+  if (length == 0 || length > BUF_LEN) {
+	  errno = EMSGSIZE;
+	  return -1;
+  }
+  for (int i=0; i < length; i++) {
+	  if (get_user(the_message[i], &buffer[i]) != 0)
+		  return -1;
+  }
+  // on success
+  for (int i=0; i < length; i++) {
+	  file->private_data->current_channel->message[i] = the_message[i];
+  }
+  file->private_data->current_channel->message_length = length;
+  return length;
+  }
 }
 
-//----------------------------------------------------------------
 static long device_ioctl( struct   file* file,
                           unsigned int   ioctl_command_id,
-                          unsigned long  ioctl_param )
+                          unsigned int  channel_id )
 {
-  // Switch according to the ioctl called
-  if( IOCTL_SET_ENC == ioctl_command_id )
+  if( MSG_SLOT_CHANNEL != ioctl_command_id || channel_id == 0 )
   {
-    // Get the parameter given to ioctl by the process
-    printk( "Invoking ioctl: setting encryption "
-            "flag to %ld\n", ioctl_param );
-    encryption_flag = ioctl_param;
+	errno = EINVAL;
+    return FAILURE;
   }
-
+  int minor = file->private_data->minor;
+  struct channel_node node = message_slot_channels[minor-1];
+  while (node != NULL) {
+	  if (node->data->id == channel_id) {
+		  file->private_data->current_channel = node;
+		  return SUCCESS;
+	  }
+	  node = node->next;
+  }
+  node = kmalloc(sizeof(channel), GFP_KERNEL);
+  node->data->id = channel_id;
+  node->data->message = NULL;
+  node->data->message_length = 0;
+  file->private_data->current_channel = node;
   return SUCCESS;
 }
 
 //==================== DEVICE SETUP =============================
 
-// This structure will hold the functions to be called
-// when a process does something to the device we created
 struct file_operations Fops =
 {
   .owner	  = THIS_MODULE, 
@@ -138,14 +148,9 @@ struct file_operations Fops =
   .release        = device_release,
 };
 
-//---------------------------------------------------------------
-// Initialize the module - Register the character device
 static int __init simple_init(void)
 {
   int rc = -1;
-  // init dev struct
-  memset( &device_info, 0, sizeof(struct chardev_info) );
-  spin_lock_init( &device_info.lock );
 
   // Register driver capabilities. Obtain major num
   rc = register_chrdev( MAJOR_NUM, DEVICE_RANGE_NAME, &Fops );
@@ -157,28 +162,27 @@ static int __init simple_init(void)
                        DEVICE_FILE_NAME, MAJOR_NUM );
     return rc;
   }
-
-  printk( "Registeration is successful. ");
-  printk( "If you want to talk to the device driver,\n" );
-  printk( "you have to create a device file:\n" );
-  printk( "mknod /dev/%s c %d 0\n", DEVICE_FILE_NAME, MAJOR_NUM );
-  printk( "You can echo/cat to/from the device file.\n" );
-  printk( "Dont forget to rm the device file and "
-          "rmmod when you're done\n" );
-
-  return 0;
+  // initialization all linked-list of channels for all possible message_slot to NULL
+  for (int i=0; i<256; i++) {
+	  message_slot_channels[i] = NULL;
+  }
+  return SUCCESS;
 }
 
-//---------------------------------------------------------------
 static void __exit simple_cleanup(void)
 {
-  // Unregister the device
-  // Should always succeed
+  struct channel_node* head;
+  struct channel_node* tmp;
+  for (int i=0; i<256; i++) {
+	  head = message_slot_channels[i];
+	  while(head != NULL) {
+		  tmp = head;
+		  head = head->next;
+		  kfree(tmp);
+	  }
+  }
   unregister_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME);
 }
 
-//---------------------------------------------------------------
 module_init(simple_init);
 module_exit(simple_cleanup);
-
-//========================= END OF FILE =========================
