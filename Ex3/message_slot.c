909752d0c9e62d15bdab2b6ac05ebd1d5d2a4166
Tomer Fisher <fisher.tomer@gmail.com>
2022-05-01 22:56:28 +0300

    fix last things

--- Ex3/message_slot.c
+++ Ex3/message_slot.c
@@ -27,13 +27,8 @@ struct channel_node {
   struct channel_node* next;
 };
 
-struct file_info {
-  int minor;
-	long channel_id;
-  struct channel_node* channel;
-};
-
-struct channel_node* message_slot_channels[256];
+struct channel_node* list_of_channels[256];
+struct channel_node* current_channels[256];
 
 // The message the device will give when asked
 static char the_message[BUF_LEN];
@@ -41,113 +36,100 @@ static char the_message[BUF_LEN];
 //================== DEVICE FUNCTIONS ===========================
 static int device_open(struct inode* inode, struct file* file)
 {
-  int minor = iminor(inode);
-  struct file_info* info = kmalloc(sizeof(struct file_info), GFP_KERNEL);
+  //unsigned int* minor = kmalloc(sizeof(int), GFP_KERNEL);
+  //int minor = iminor(inode);
+  int* minor_pointer;
   printk("Invoking device_open(%p)\n", file);
-  if (info == NULL) {
-	  return FAILURE;
-  }
-  info->minor = minor;
-  info->channel_id = 0;
-  //info->channel = NULL;
-  file->private_data = (void*)info;
+  minor_pointer = kmalloc(sizeof(int), GFP_KERNEL);
+  if (minor_pointer == NULL)
+    return FAILURE;
+  *minor_pointer = iminor(inode);
+  file->private_data = (void*)minor_pointer;
   printk("device_open(%p) successfully completed\n", file);
   return SUCCESS;
 }
 
-static int device_release(struct inode* inode, struct file*  file) {
-	printk("Invoking device_release(%p,%p)\n", inode, file);
+static int device_release(struct inode* inode, struct file* file) {
   kfree(file->private_data);
-  printk("device_release(%p,%p) successfully completed\n", inode, file);
-	return SUCCESS;
+  return SUCCESS;
 }
 
 static ssize_t device_read( struct file* file, char __user* buffer, size_t length, loff_t* offset)
 {
-  int i, message_length;
-  struct file_info* info = (struct file_info*)(file->private_data);
-  char* last_message;
+  //int minor = file->private_data, i;
+  int minor = *(int*)file->private_data, i;
   printk("Invoking device_read(%p,%ld)\n", file, length);
-  if (info->channel_id == 0)
+  if(current_channels[minor-1] == NULL)
     return -EINVAL;
-  last_message = info->channel->message;
-  message_length = info->channel->message_length;
-  if (message_length == 0)
+  if (current_channels[minor-1]->message_length == 0)
     return -EWOULDBLOCK;
-  if (length < message_length)
+  if (length < current_channels[minor-1]->message_length)
     return -ENOSPC;
-  for (i = 0; i < message_length; i++) {
-	  if (put_user(last_message[i], buffer + i) != 0)
+  for (i = 0; i < current_channels[minor-1]->message_length; i++) {
+	  if (put_user(current_channels[minor-1]->message[i], buffer + i) != 0)
 		  return FAILURE;
   }
-  printk("device_read(%p,%ld) successfully completed (last written - %s)\n", file, length, last_message);
-  return info->channel->message_length;
+  printk("device_read(%p,%ld) successfully completed (last written - %s)\n", file, length, current_channels[minor-1]->message);
+  return current_channels[minor-1]->message_length;
 }
 
 static ssize_t device_write(struct file* file, const char __user* buffer, size_t length, loff_t* offset)
 {
-  int i=0;
-  struct file_info* info = (struct file_info*)(file->private_data);
+  //int minor = file->private_data, i;
+  int minor = *(int*)file->private_data, i;
   printk("Invoking device_write(%p,%ld)\n", file, length);
-  if(info->channel_id == 0) {
+  if(current_channels[minor-1] == NULL)
 	  return -EINVAL;
-  }
-  if (length == 0 || length > BUF_LEN) {
+  if (length == 0 || length > BUF_LEN)
 	  return -EMSGSIZE;
-  }
   for (i=0; i < length; i++) {
 	  if (get_user(the_message[i], &buffer[i]) != 0)
 		  return -1;
   }
-  // on success
   for (i=0; i < length; i++) {
-	  info->channel->message[i] = the_message[i];
+    current_channels[minor-1]->message[i] = the_message[i];
   }
-  info->channel->message_length = length;
+  current_channels[minor-1]->message_length = length;
   printk("device_write(%p,%ld) successfully completed\n", file, length);
   return length;
 }
 
 static long device_ioctl(struct file* file, unsigned int ioctl_command_id, unsigned long channel_id)
 {
-  struct file_info* info = (struct file_info*)(file->private_data);
-  struct channel_node* head = message_slot_channels[info->minor-1];
-  struct channel_node* node;
+  //int minor = file->private_data;
+  int minor = *(int*)file->private_data;
+  struct channel_node* node = list_of_channels[minor-1];
   printk("Invoking device_ioctl(%p,%ld)\n", file, channel_id);
   if (MSG_SLOT_CHANNEL != ioctl_command_id || channel_id == 0)
     return -EINVAL;
-  if (message_slot_channels[info->minor-1] == NULL) {
+  if (node == NULL) {
     node = kmalloc(sizeof(struct channel_node), GFP_KERNEL);
     node->id = channel_id;
     node->message_length = 0;
     node->next = NULL;
-    info->channel_id = channel_id;
-    info->channel = node;
-    message_slot_channels[info->minor-1] = node;
+    current_channels[minor-1] = node;
+    list_of_channels[minor-1] = node;
     printk("device_ioctl(%p,%ld) successfully completed - create new channel node\n", file, channel_id);
     return SUCCESS;
   }
-  node = head;
   while (node->next != NULL) {
 	  if (node->id == channel_id) {
-		  info->channel_id = channel_id;
-      info->channel = node;
+      current_channels[minor-1] = node;
       printk("device_ioctl(%p,%ld) successfully completed - found channel node\n", file, channel_id);
 		  return SUCCESS;
 	  }
 	  node = node->next;
   }
   if (node->id == channel_id) {
-	  info->channel_id = channel_id;
-    info->channel = node;
+	  current_channels[minor-1] = node;
     printk("device_ioctl(%p,%ld) successfully completed - found channel node\n", file, channel_id);
 	  return SUCCESS;
   }
   node->next = kmalloc(sizeof(struct channel_node), GFP_KERNEL);
   node->next->id = channel_id;
   node->next->message_length = 0;
-  info->channel_id = channel_id;
-  info->channel = node->next;
+  node->next->next = NULL;
+  current_channels[minor-1] = node->next;
   printk("device_ioctl(%p,%ld) successfully completed - create new channel node\n", file, channel_id);
   return SUCCESS;
 }
@@ -180,7 +162,8 @@ static int __init simple_init(void)
   }
   // initialization all linked-list of channels for all possible message_slot to NULL
   for (i=0; i<256; i++) {
-	  message_slot_channels[i] = NULL;
+	  list_of_channels[i] = NULL;
+    current_channels[i] = NULL;
   }
   printk("Registeration Successfully Completed.");
   return SUCCESS;
@@ -192,7 +175,7 @@ static void __exit simple_cleanup(void)
   struct channel_node* head;
   struct channel_node* tmp;
   for (i=0; i<256; i++) {
-	  head = message_slot_channels[i];
+	  head = list_of_channels[i];
 	  while(head != NULL) {
 		  tmp = head;
 		  head = head->next;
